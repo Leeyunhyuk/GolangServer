7f66d9301ba55387f8d476acbf0479a8db566a96
leeyunhyuk <leeyunhyuk@gmail.com>
2024-05-15 23:14:25 +0900

    Feat : Gin 프레임워크 사용

--- init/cmd/cmd.go
+++ init/cmd/cmd.go
@@ -2,21 +2,30 @@ package cmd
 
 import (
 	"BACKEND_GO/config"
+	"BACKEND_GO/network"
 	"fmt"
 )
 
 // repository, service, types, config 항목을 Cmd 항목에 담을 것
 type Cmd struct {
-	config *config.Config
+	config  *config.Config
+	network *network.Network
 }
 
 // main 에서 이 NewCmd 함수를 통해 값을 받아서 서버를 구동 할 예정
 func NewCmd(filePath string) *Cmd {
+
+	fmt.Println("여기는 뜹니다")
+
 	c := &Cmd{
-		config: config.NewConfig(filePath),
+		config:  config.NewConfig(filePath),
+		network: network.NewNetwork(),
 	}
+	//이 아래는 동작을 안함
 
 	fmt.Println(c.config.Server.Port)
+
+	c.network.ServerStart(c.config.Server.Port)
 
 	return c
 }
--- network/root.go
+++ network/root.go
@@ -1,3 +1,20 @@
 package network
 
+import "github.com/gin-gonic/gin"
+
 //api에 대한 router 설정을 하고 root.go를 통해 api 관리를 root만 cmd에 넣어줘서 관리
+
+type Network struct {
+	engin *gin.Engine
+}
+
+func NewNetwork() *Network {
+	r := &Network{
+		engin: gin.New(),
+	}
+	return r
+}
+
+func (n *Network) ServerStart(port string) error {
+	return n.engin.Run(port)
+}
